8a08d2879eed0126f587e817aad1579c73c882d9
Jamal <jmohammadi1987@gmail.com>
2026-04-26 20:25:17 -0400

    add BeverageBuilder, Order, and OrderDriver

--- src/BeverageBuilder.java
+++ src/BeverageBuilder.java
@@ -1,2 +1,31 @@
 public class BeverageBuilder {
+    private Beverage beverage;
+
+    public BeverageBuilder(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    public BeverageBuilder setSize(String size) {
+        beverage.setSize(size);
+        return this;
+    }
+
+    public BeverageBuilder setMilk(String milk) {
+        beverage.setMilk(milk);
+        return this;
+    }
+
+    public BeverageBuilder setShots(int shots) {
+        beverage.setShots(shots);
+        return this;
+    }
+
+    public BeverageBuilder setSweetener(String sweetener) {
+        beverage.setSweetener(sweetener);
+        return this;
+    }
+
+    public Beverage build() {
+        return beverage;
+    }
 }
